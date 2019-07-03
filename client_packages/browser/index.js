71aa2c04faf49d7dd05dc088f410d2fffc5e0555
Данил Аникьев <anikyev95@mail.ru>
2019-07-03 18:55:04 +0400

    Base

---
+++ client_packages/browser/index.js
@@ -0,0 +1,27 @@
+let browser = mp.browsers.new("package://ragerp/browser/index.html");
+
+mp.callCEFR = function (eventName, args) { 
+    let argumentsString = '';
+
+    args.forEach(arg => {
+        switch(typeof arg) {
+            case 'string': {
+                argumentsString += `, '${arg}'`;
+                break;
+            }
+            case 'number': {
+                argumentsString += `, ${arg}`;
+                break;
+            }
+            case 'boolean': {
+                argumentsString += `, ${arg}`;
+                break;
+            }
+            case 'object': {
+                argumentsString += `, ${JSON.stringify(arg)}`;
+                break;
+            }
+        }
+    });
+    browser.execute(`mp.events.call('${eventName}'${argumentsString});`);
+}
--- packages/base/index.js
+++ packages/base/index.js
@@ -1,1 +1,13 @@
-require('./base')
+let fs = require('fs');
+let path = require('path');
+
+
+let db = require('./db');
+
+db.connect(function() {
+    fs.readdirSync(path.dirname(__dirname)).forEach(file => {
+        file != 'base' && mp.events.add(require('../' + file + '/events'));
+    });
+
+    mp.events.call('init');
+});
