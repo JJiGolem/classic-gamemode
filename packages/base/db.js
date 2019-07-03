42e7e06b5f0e00fa689348a13545dfe262eabacc
Данил Аникьев <anikyev95@mail.ru>
2019-07-03 23:13:11 +0400

    Ignore modules in DB

--- packages/base/db.js
+++ packages/base/db.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
 const fs = require("fs");
 
+
+let ignoreModules = require('./ignoreModules');
+
 module.exports = {
     sequelize: null,
     Models: {},
@@ -17,17 +20,19 @@ module.exports = {
     loadModels: function() {
         console.log("[DATABASE] load models...");
         fs.readdirSync(path.dirname(__dirname)).forEach(dir => {
-            if (dir != 'base' && fs.existsSync(path.dirname(__dirname)+ "/" + dir + '/db')) {
+            if (dir != 'base' && !ignoreModules.includes(dir) && fs.existsSync(path.dirname(__dirname)+ "/" + dir + '/db')) {
+                console.log(`[DATABASE] -${dir}`);
                 fs.readdirSync(path.dirname(__dirname)+ "/" + dir + '/db').forEach(file => {
-                    // let model = this.sequelize.import(path.dirname(__dirname)+ "/" + dir + '/db/' + file);
-                    // this.Models[model.name] = model;
+                    console.log(`[DATABASE] ---${file}`);
+                    let model = this.sequelize.import(path.dirname(__dirname)+ "/" + dir + '/db/' + file);
+                    this.Models[model.name] = model;
                 });
             }
         });
-        // for (var name in this.Models) {
-        //     var model = this.Models[name];
-        //     if (model.associate) model.associate(this.Models);
-        // }
-        // this.sequelize.sync();
+        for (var name in this.Models) {
+            var model = this.Models[name];
+            if (model.associate) model.associate(this.Models);
+        }
+        this.sequelize.sync();
     }
 }
