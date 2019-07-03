08085be33cd8b4be7c8d9c745b566f428f2dc08b
Данил Аникьев <anikyev95@mail.ru>
2019-07-03 19:17:32 +0400

    database init

--- packages/base/db.js
+++ packages/base/db.js
@@ -18,16 +18,16 @@ module.exports = {
         console.log("[DATABASE] load models...");
         fs.readdirSync(path.dirname(__dirname)).forEach(dir => {
             if (dir != 'base') {
-                fs.readdirSync('../' + dir + '/db').forEach(file => {
-                    let model = this.sequelize.import();
-                    this.Models[model.name] = model;
+                fs.readdirSync(path.dirname(__dirname)+ "/" + dir + '/db').forEach(file => {
+                    // let model = this.sequelize.import(path.dirname(__dirname)+ "/" + dir + '/db/' + file);
+                    // this.Models[model.name] = model;
                 });
             }
         });
-        for (var name in this.Models) {
-            var model = this.Models[name];
-            if (model.associate) model.associate(this.Models);
-        }
-        this.sequelize.sync();
+        // for (var name in this.Models) {
+        //     var model = this.Models[name];
+        //     if (model.associate) model.associate(this.Models);
+        // }
+        // this.sequelize.sync();
     }
 }
