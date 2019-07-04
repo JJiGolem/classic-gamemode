3bd620e01b2dc77fd61892417b1e23049329826f
Kirill Savchenko <kirsav10@gmail.com>
2019-07-05 01:24:48 +0400

    онлайн игроков в худе

--- packages/hud/events.js
+++ packages/hud/events.js
@@ -1,6 +1,15 @@
 module.exports = {
-
+    /// Отображение онлайна в худе
     "playerJoin": (player) => {
-        player.call('hud.load');
+        player.call('hud.load'); 
+
+        mp.players.forEach((current) => {
+            current.call("hud.setData", [{players: mp.players.length}]);
+        });
+    },
+    "playerQuit": (player) => {
+        mp.players.forEach((current) => {
+            current.call("hud.setData", [{players: mp.players.length}]);
+        });
     }
 }
