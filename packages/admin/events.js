f15a80bd8bbc1dc7122b52fca618fbf72074b4f6
Данил Аникьев <anikyev95@mail.ru>
2019-07-06 10:36:38 +0400

    Func descriptions. Notes for swift

--- packages/admin/events.js
+++ packages/admin/events.js
@@ -1,9 +1,13 @@
+"use strict";
+let admin = require("./index.js");
 let commands = {};
 
 module.exports = {
     "init": () => {
-        commands = require("./index.js").init();
+        /// Список всех команд на сервере
+        commands = admin.init();
     },
+    // todo описание(описание пишется с 3 слешами, заметки по коду по типу туду с 2 слешами)
     "admin.command.handle": (player, command, args) => {
         if (command == "/ahelp") return mp.events.call('admin.command.help', player, args);
         let cmd = commands[command];
@@ -13,6 +17,7 @@ module.exports = {
 
         cmd.handler(player, args);
     },
+    // todo описание
     "admin.command.help": (player, args) => {
         if (!args[0] || isNaN(parseInt(args[0]))) return player.call('chat.message.push', [`!{#ffffff} Используйте /ahelp [уровень администрирования]`]);
         //TODO: Проверка на уровень админки
