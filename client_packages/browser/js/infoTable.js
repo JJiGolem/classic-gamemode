0f76e6e8450e9a527c1b45a80f1e7bdfe0e5129c
carter <dj-ratmir@mail.ru>
2019-07-07 23:43:31 +0300

    InfoTable на VUE.

---
+++ client_packages/browser/js/infoTable.js
@@ -0,0 +1,55 @@
+var infoTable = new Vue({
+    el: "#infoTable",
+    data: {
+        show: false,
+        // Доступные таблицы с информацией
+        tables: {
+            "character_skills": {
+                header: "Навыки",
+                params: [{
+                        key: "Рыбак",
+                        value: 0,
+                        isProgress: true,
+                    },
+                    {
+                        key: "Фермер",
+                        value: 0,
+                        isProgress: true,
+                    },
+                    {
+                        key: "Водитель автобуса",
+                        value: 0,
+                        isProgress: true,
+                    },
+                    {
+                        key: "Таксист",
+                        value: 0,
+                        isProgress: true,
+                    },
+                    {
+                        key: "Дальнобойщик",
+                        value: 0,
+                        isProgress: true,
+                    },
+                ]
+            }
+        },
+        // Текущая информация
+        name: "character_skills"
+    },
+    methods: {
+        prettyValue(param) {
+            var value = param.value;
+            if (param.isProgress) value += '%';
+            return value;
+        },
+        setValues(name, values) {
+            var table = this.tables[name];
+            if (!table) return;
+            if (typeof values == 'string') values = JSON.parse(values);
+            for (var i = 0; i < values.length; i++) {
+                table.params[i].value = values[i];
+            }
+        },
+    }
+});
