f599b40204b57a94628dd9b795cb3a80aee066a5
Kirill Savchenko <kirsav10@gmail.com>
2019-07-10 23:16:11 +0400

    фикс счетчика онлайна

--- packages/hud/events.js
+++ packages/hud/events.js
@@ -2,14 +2,13 @@ module.exports = {
     /// Отображение онлайна в худе
     "playerJoin": (player) => {
         player.call('hud.load'); 
-
         mp.players.forEach((current) => {
             current.call("hud.setData", [{players: mp.players.length}]);
         });
     },
     "playerQuit": (player) => {
         mp.players.forEach((current) => {
-            current.call("hud.setData", [{players: mp.players.length}]);
+            current.call("hud.setData", [{players: mp.players.length - 1}]); /// После выхода из игры игрок какое-то время висит в пуле, поэтому передаем на 1 меньше
         });
     }
 }
