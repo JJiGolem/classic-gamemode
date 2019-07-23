ae7c578b93e096902e3a9edc7d2054b5d88106b0
Vetal <dj-ratmir@mail.ru>
2019-07-23 16:30:21 +0300

    Файлы characterSelector объединены с файлами characterInfo.

--- client_packages/browser/js/characterInfo.js
+++ client_packages/browser/js/characterInfo.js
@@ -43,3 +43,27 @@ characterInfo.addCharacter({
     warns: 0
 });
 characterInfo.show = true;*/
+
+var characterSelector = new Vue({
+    el: "#characterSelector",
+    methods: {
+        left() {
+            if (loader.show) return;
+            mp.trigger("characterInit.chooseLeft");
+        },
+        enter() {
+            if (loader.show) return;
+            loader.show = true;
+            mp.trigger("characterInit.choose");
+        },
+        right() {
+            if (loader.show) return;
+            mp.trigger("characterInit.chooseRight");
+        }
+    },
+    computed: {
+        show() {
+            return characterInfo.show && characterInfo.characters.length;
+        }
+    }
+});
