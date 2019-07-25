985979296cb7bc05355711a5412ed537143704b7
Kirill Savchenko <kirsav10@gmail.com>
2019-07-26 02:22:58 +0400

    new speedometer ui

--- client_packages/carshow/index.js
+++ client_packages/carshow/index.js
@@ -6,7 +6,7 @@ mp.events.add('carshow.list.show', (inputList) => {
     let sceneryCamera = mp.cameras.new('default', new mp.Vector3(-44 - 4, -1098 - 4, 25 + 2.5), new mp.Vector3(0, 0, 0), 70);
     //31.673555374145508
     //126.1431884765625
-    sceneryCamera.pointAtCoord(-44, -1098, 25); //Changes the rotation of the camera to point towards a location
+    sceneryCamera.pointAtCoord(-44, -1098, 25);
     sceneryCamera.setActive(true);
     mp.game.cam.renderScriptCams(true, false, 0, true, false);
 
@@ -15,42 +15,56 @@ mp.events.add('carshow.list.show', (inputList) => {
     for (var j = 0; j < list.length; j++) {
         mp.chat.debug(`${j}Модель: ${list[j].vehiclePropertyModel} Количество: ${list[j].count} ${list[j].properties.maxFuel}`);
     }
+
+    let models = []; 
+    list.forEach((current)=> {
+        models.push(current.properties.name);
+    })
     mp.callCEFV(`selectMenu.menu = selectMenu.menus["carShowMenu"]`);
+    mp.callCEFVN({"selectMenu.menu.items[0].values": models});
     mp.callCEFV(`selectMenu.open()`);
-    //mp.callCEFVN({ "selectMenu.show": "true" });
 }
 );
-mp.keys.bind(0x25, true, function () {
-    showPreviousCar();
-});
 
-mp.keys.bind(0x27, true, function () {
-    showNextCar();
-});
-
-function showNextCar() {
+mp.events.add('carshow.vehicle.show', (i)=> {
     current.destroy();
-    if (i == list.length - 1) {
-        i = 0;
-        current = mp.vehicles.new(mp.game.joaat(list[i].vehiclePropertyModel), new mp.Vector3(-44.08749008178711, -1098.660400390625, 25.74812889099121));
-    } else {
-        current = mp.vehicles.new(mp.game.joaat(list[i + 1].vehiclePropertyModel), new mp.Vector3(-44.08749008178711, -1098.660400390625, 25.74812889099121));
-        i++;
-    }
+    current = mp.vehicles.new(mp.game.joaat(list[i].vehiclePropertyModel), new mp.Vector3(-44.08749008178711, -1098.660400390625, 25.74812889099121));
     current.setHeading(115);
-}
+});
+// function showCar(i) {
 
-function showPreviousCar() {
-    current.destroy();
-    if (i == 0) {
-        i = list.length - 1;
-        current = mp.vehicles.new(mp.game.joaat(list[i].vehiclePropertyModel), new mp.Vector3(-44.08749008178711, -1098.660400390625, 25.74812889099121));
-    } else {
-        current = mp.vehicles.new(mp.game.joaat(list[i - 1].vehiclePropertyModel), new mp.Vector3(-44.08749008178711, -1098.660400390625, 25.74812889099121));
-        i--;
-    }
-    current.setHeading(115);
-}
+// }
+// mp.keys.bind(0x25, true, function () {
+//     showPreviousCar();
+// });
+
+// mp.keys.bind(0x27, true, function () {
+//     showNextCar();
+// });
+
+// function showNextCar() {
+//     current.destroy();
+//     if (i == list.length - 1) {
+//         i = 0;
+//         current = mp.vehicles.new(mp.game.joaat(list[i].vehiclePropertyModel), new mp.Vector3(-44.08749008178711, -1098.660400390625, 25.74812889099121));
+//     } else {
+//         current = mp.vehicles.new(mp.game.joaat(list[i + 1].vehiclePropertyModel), new mp.Vector3(-44.08749008178711, -1098.660400390625, 25.74812889099121));
+//         i++;
+//     }
+//     current.setHeading(115);
+// }
+
+// function showPreviousCar() {
+//     current.destroy();
+//     if (i == 0) {
+//         i = list.length - 1;
+//         current = mp.vehicles.new(mp.game.joaat(list[i].vehiclePropertyModel), new mp.Vector3(-44.08749008178711, -1098.660400390625, 25.74812889099121));
+//     } else {
+//         current = mp.vehicles.new(mp.game.joaat(list[i - 1].vehiclePropertyModel), new mp.Vector3(-44.08749008178711, -1098.660400390625, 25.74812889099121));
+//         i--;
+//     }
+//     current.setHeading(115);
+// }
 
 mp.events.add("carshow.car.buy", (carId) => {
     mp.events.callRemote('carshow.car.buy', carId);
