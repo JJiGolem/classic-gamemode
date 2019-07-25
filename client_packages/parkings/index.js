90cdac98da7e2787b13b566dca4993d9a6cd4703
Kirill Savchenko <kirsav10@gmail.com>
2019-07-25 23:46:02 +0400

    Временный фикс selectMenu

--- client_packages/parkings/index.js
+++ client_packages/parkings/index.js
@@ -3,14 +3,11 @@ var currentParkingId;
 mp.events.add('parkings.menu.show', (parkingId) => {
     currentParkingId = parkingId;
     mp.callCEFV(`selectMenu.menu = selectMenu.menus["parkingMenu"]`);
-    mp.callCEFV(`selectMenu.menu.i = 0`); // temp ?
-    mp.callCEFV(`selectMenu.menu.j = 0`);
-    mp.callCEFVN({ "selectMenu.show": true });
+    mp.callCEFV(`selectMenu.open()`);
 });
 
 mp.events.add('parkings.menu.close', () => {
-    mp.callCEFV(`selectMenu.menu = null`); // temp ?
-    mp.callCEFVN({ "selectMenu.show": false });
+    mp.callCEFV(`selectMenu.close()`);
 });
 
 mp.events.add('parkings.vehicle.get', () => {
