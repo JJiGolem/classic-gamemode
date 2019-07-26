3de0de7470b192cb2de458bc4ea477baec725a8c
Данил Аникьев <anikyev95@mail.ru>
2019-07-27 00:43:42 +0400

    Debug voice

--- packages/voiceChat/events.js
+++ packages/voiceChat/events.js
@@ -4,11 +4,13 @@ let voice = require("./index.js");
 module.exports = {
     "voiceChat.add": (player, target) => {
         if(target) {
+            console.log("voiceChat.add - p_id: " + player.id + "; t_id: " + target.id);
             player.enableVoiceTo(target);
         }
     },
     "voiceChat.remove": (player, target) => {
         if(target) {
+            console.log("voiceChat.remove - p_id: " + player.id + "; t_id: " + target.id);
             player.disableVoiceTo(target);
         }
     }
