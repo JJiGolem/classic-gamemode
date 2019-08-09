c114662d6ddd5a3b545b4268386d6a749b7f5793
Kirill Savchenko <kirsav10@gmail.com>
2019-08-09 21:19:38 +0400

    fixes

--- packages/time/index.js
+++ packages/time/index.js
@@ -4,8 +4,9 @@ setInterval(()=>{
 
     let date = new Date();
 
-    mp.world.time.hour = date.getHours();
-    mp.world.time.minute = date.getMinutes();
-    mp.world.time.second = date.getSeconds();
+    // mp.world.time.hour = date.getHours();
+    // mp.world.time.minute = date.getMinutes();
+    // mp.world.time.second = date.getSeconds();
 
+    mp.world.time.hour = 19;
 }, 1000);
