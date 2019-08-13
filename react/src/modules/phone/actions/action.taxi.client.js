dac5719a9dd34fd92b333eb29ab16b2eed5fed58
deusmemes <maxin29999@gmail.com>
2019-08-14 03:04:05 +0400

    taxi app test version

---
+++ react/src/modules/phone/actions/action.taxi.client.js
@@ -0,0 +1,17 @@
+export const loadLocationTaxiClient = (area, street) => ({
+    type: 'LOAD_LOCATION_TAXI_CLIENT',
+    payload: { area, street }
+});
+
+export const createOrderTaxiClient = () => ({
+    type: 'CREATE_ORDER_TAXI_CLIENT'
+});
+
+export const cancelOrderTaxiClient = () => ({
+   type: 'CANCEL_ORDER_TAXI_CLIENT'
+});
+
+export const ansTaxiClient = answer => ({
+   type: 'ANS_TAXI_CLIENT',
+   payload: answer
+});
---
+++ react/src/modules/phone/reducers/reducer.taxi.cleint.js
@@ -0,0 +1,34 @@
+const initialState = {};
+
+export default function taxiClient(state = initialState, action) {
+    const {type, payload} = action;
+
+    switch (type) {
+        case 'LOAD_LOCATION_TAXI_CLIENT':
+            return {
+              ...state,
+              location: payload
+            };
+
+        case 'ANS_TAXI_CLIENT':
+            const newState = { ...state };
+            newState.isSearch = false;
+            newState.answer = payload;
+            return newState;
+
+        case 'CREATE_ORDER_TAXI_CLIENT':
+            return {
+                ...state,
+                isSearch: true
+            };
+
+        case 'CANCEL_ORDER_TAXI_CLIENT':
+            return {
+                ...state,
+                isSearch: false,
+                answer: null
+            };
+    }
+
+    return state;
+}
