7b11723230fff3ac862fec8456ee494a475d8c93
Vetal <dj-ratmir@mail.ru>
2019-08-18 16:20:00 +0300

    [factions] Невозможность сесть в авто организации.

--- packages/factions/events.js
+++ packages/factions/events.js
@@ -1,4 +1,5 @@
 let factions = require('./index.js');
+let notifs = require('../notifications');
 module.exports = {
     "init": () => {
         factions.init();
@@ -8,5 +9,12 @@ module.exports = {
     },
     "factions.warehouse.putBox": (player) => {
         factions.putBox(player);
+    },
+    "playerEnterVehicle": (player, vehicle, seat) => {
+        if (seat != -1 || vehicle.key != 'faction' || !player.character.factionId) return;
+        if (player.character.factionId != vehicle.owner) {
+            notifs.error(player, `Вы не состоите в организации`, factions.getFaction(vehicle.owner).name);
+            player.removeFromVehicle();
+        }
     },
 };
