6a469f3912101f0068cbb7b1e6954f00fdad0882
Vetal <dj-ratmir@mail.ru>
2019-09-12 22:29:34 +0300

    [death] Вызов медикам.

--- packages/death/events.js
+++ packages/death/events.js
@@ -9,12 +9,14 @@ module.exports = {
         player.spawn(player.position);
         player.health = death.health;
         player.setVariable("knocked", true);
+        mp.events.call(`mapCase.ems.calls.add`, player, `Ранение`);
     },
     "death.spawn": (player) => {
         // TODO: спавн в больнице
         player.spawn(player.position);
         player.health = 10;
         player.setVariable("knocked", false);
+        mp.events.call(`mapCase.ems.calls.remove`, player, player.character.id);
     },
     "playerDeath": (player, reason, killer) => {
 
