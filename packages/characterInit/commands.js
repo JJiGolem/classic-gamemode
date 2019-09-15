1747ba08448b60d21a72c082b7d72e9de8d62a6d
Kirill Savchenko <kirsav10@gmail.com>
2019-09-15 14:38:36 +0400

    [characterInit] Фикс функции выдачи одежды

--- packages/characterInit/commands.js
+++ packages/characterInit/commands.js
@@ -3,6 +3,7 @@ let init = require('./index.js');
 module.exports = {
     '/sclothes': {
         args: '',
+        description: 'Выдача стартовой одежды',
         access: 6,
         handler: (player, args) => {
             init.setStartClothes(player);
