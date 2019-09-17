db55b0a4c9b597a679c6d8c15262edf7996c3825
deusmemes <maxin29999@gmail.com>
2019-09-17 15:51:33 +0400

    [Hud] add search

--- client_packages/browser/js/playersList.js
+++ client_packages/browser/js/playersList.js
@@ -1,11 +1,40 @@
 var playersList = new Vue({
     el: '#playersList',
     data: {
-        show: false,
-        players: [],
-        loader: false
+        show: true,
+        players: [
+            {
+                name: 'Dun Hill',
+                faction: 'LSPD',
+                ping: 9
+            },
+            {
+                name: 'Swifty Swift',
+                faction: '-',
+                ping: 5
+            },
+            {
+                name: 'Deus Memes',
+                faction: '-',
+                ping: 9
+            },
+            {
+                name: 'Sean Muller',
+                faction: 'LSPD',
+                ping: 9
+            },
+            {
+                name: 'Sean Hans',
+                faction: 'EMS',
+                ping: 9
+            },
+        ],
+        loader: false,
+        word: ''
     },
     methods: {
-
+        getPlayers(type) {
+            return this.players.filter(player => player[type].toString().startsWith(this.word));
+        }
     }
 })
