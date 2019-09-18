1aa6c8dfa528cbcd7b9cd91072113b92b99c81f9
deusmemes <maxin29999@gmail.com>
2019-09-18 16:58:23 +0400

    [Hud] add search & sort in players list

--- client_packages/browser/js/playersList.js
+++ client_packages/browser/js/playersList.js
@@ -1,40 +1,60 @@
 var playersList = new Vue({
     el: '#playersList',
     data: {
-        show: true,
+        show: false,
         players: [
-            {
-                name: 'Dun Hill',
-                faction: 'LSPD',
-                ping: 9
-            },
-            {
-                name: 'Swifty Swift',
-                faction: '-',
-                ping: 5
-            },
-            {
-                name: 'Deus Memes',
-                faction: '-',
-                ping: 9
-            },
-            {
-                name: 'Sean Muller',
-                faction: 'LSPD',
-                ping: 9
-            },
-            {
-                name: 'Sean Hans',
-                faction: 'EMS',
-                ping: 9
-            },
+            // {
+            //     id: 1,
+            //     name: 'Dun Hill',
+            //     faction: 'LSPD',
+            //     ping: 9
+            // },
+            // {
+            //     id: 2,
+            //     name: 'Swifty Swift',
+            //     faction: '-',
+            //     ping: 5
+            // },
+            // {
+            //     id: 3,
+            //     name: 'Deus Memes',
+            //     faction: '-',
+            //     ping: 9
+            // },
+            // {
+            //     id: 4,
+            //     name: 'Sean Muller',
+            //     faction: 'LSPD',
+            //     ping: 9
+            // },
+            // {
+            //     id: 5,
+            //     name: 'Sean Hans',
+            //     faction: 'EMS',
+            //     ping: 9
+            // },
         ],
         loader: false,
-        word: ''
+        search: '',
+        type: 'name',
+        sortedBy: 'id',
+        options: [
+            { text: 'id', value: 'id' },
+            { text: 'Имя', value: 'name' },
+            { text: 'Организация', value: 'faction' }
+        ]
     },
     methods: {
-        getPlayers(type) {
-            return this.players.filter(player => player[type].toString().startsWith(this.word));
+        getPlayers() {
+            return this.players
+                .sort((a, b) => a[this.sortedBy].toString().localeCompare(b[this.sortedBy].toString()))
+                .filter(player => player[this.type]
+                    .toString()
+                    .toLowerCase()
+                    .startsWith(this.search.toString().toLowerCase()))
+        },
+        setSortedBy(newSortedBy) {
+            this.sortedBy = newSortedBy;
         }
     }
 })
