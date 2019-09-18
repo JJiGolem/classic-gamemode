20c8424f8af9d914dc3970b8ce3768dcf2c2cd80
Данил Аникьев <anikyev95@mail.ru>
2019-09-19 01:32:01 +0400

    [admin] Remove comments from init [economy] load economic indicators from modules

--- packages/economy/index.js
+++ packages/economy/index.js
@@ -4,8 +4,41 @@
 let economicIndicators = new Array();
 module.exports = {
     async init() {
+        console.log("[ECONOMY] load economic indicators...");
+
         economicIndicators = await db.Models.EconomicIndicator.findAll();
+        
+        for (let file of fs.readdirSync(path.dirname(__dirname))) {
+            if (file != 'base' && !ignoreModules.includes(file) && fs.existsSync(path.dirname(__dirname) + "/" + file + '/index.js'))
+            {
+                let objects = require('../' + file + '/index');
+                for (const key in objects) {
+                    if (typeof objects[key] == "number" ||
+                        typeof objects[key] == "string" ||
+                        typeof objects[key] == "boolean") {
+                            let economicIndicator = economicIndicators.find(elem => elem.type == `${file}.${key}`);
+                            if (economicIndicator == null) {
+                                let economicIndicator = await db.Models.EconomicIndicator.create({
+                                    type: `${file}.${key}`,
+                                    name: `${file}.${key}`,
+                                    description: "",
+                                    value: objects[key] + ""
+                                });
+                                economicIndicators.push(economicIndicator);
+                            }
+                            else {
+                                if (typeof objects[key] == "number") objects[key] = parseFloat(economicIndicator.value);
+                                if (typeof objects[key] == "string") objects[key] = economicIndicator.value;
+                                if (typeof objects[key] == "boolean") objects[key] = economicIndicator.value == "true";
+                            }
+                    }
+                }
+            }
+        }
+
         mp.events.call("economy.done");
+
+        console.log("[ECONOMY] economic indicators loaded.");
     },
     async create(type, name, count) {
         economicIndicators.push(await db.Models.EconomicIndicator.create({type: type, name: name, count: count}));
