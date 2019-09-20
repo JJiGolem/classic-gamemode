436d63b6b9dc82477fcb998832d40481c98cf43a
Данил Аникьев <anikyev95@mail.ru>
2019-09-21 00:32:44 +0400

    [economy] remove debug command

--- packages/money/commands.js
+++ packages/money/commands.js
@@ -65,20 +65,4 @@ module.exports = {
             });
         }
     },
-    "/cashtest": {
-        access: 6,
-        description: "",
-        args: "",
-        handler: (player, args) => {
-            money.removeCash(player, parseInt(args[0]), function(result) {
-                if (result) {
-                    console.log("YES");
-                    throw new Error();
-                }
-                else {
-                    console.log("NO");
-                }
-            });
-        }
-    }
 }
