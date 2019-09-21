6c3e06e6a4f564fddd4ed6d9aaebb36ab88636ef
Данил Аникьев <anikyev95@mail.ru>
2019-09-22 01:14:12 +0400

    [biz] fix bizcreate

--- react/src/modules/phone/reducers/reducer.info.js
+++ react/src/modules/phone/reducers/reducer.info.js
@@ -185,7 +185,7 @@ const initialState = {
             ]
             // order: {
             //     productCount,
-            //     productPrice
+            //     productPrice,
             // }
         }
     ]
