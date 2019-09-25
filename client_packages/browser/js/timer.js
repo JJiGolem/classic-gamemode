78a209af0b9b25bc99eafb2b67bacf88368a859c
Vetal <dj-ratmir@mail.ru>
2019-09-25 23:39:06 +0300

    [timer] Верстка + VUE.

---
+++ client_packages/browser/js/timer.js
@@ -0,0 +1,46 @@
+var timer = new Vue({
+    el: "#timer",
+    data: {
+        // текущий таймер
+        timer: null,
+        // список таймеров
+        timers: {
+            "death": {
+                icon: "death.svg",
+                text: "До смерти осталось"
+            }
+        },
+        // общее время жизни таймера
+        allTime: 0,
+        // оставшееся время жизни таймера
+        time: 0,
+        // таймер отсчета времени
+        updateTimer: null,
+    },
+    computed: {
+        // ширина прогрессбара (от 0 до 100)
+        width() {
+            return this.time / this.allTime * 100;
+        },
+    },
+    methods: {
+        show(name, time) {
+            if (!this.timers[name]) return;
+
+            this.time = time;
+            this.allTime = time;
+            this.timer = this.timers[name];
+            clearInterval(this.updateTimer);
+            this.updateTimer = setInterval(() => {
+                this.time -= 1000;
+                if (this.time < 0) {
+                    clearInterval(this.updateTimer);
+                    this.timer = null;
+                }
+            }, 1000);
+        }
+    }
+});
+
+// for tests
+timer.show("death", 180000);
