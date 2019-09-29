08de45f591035b5fac1b458b78ab68b4a47b0f75
Carter <dj-ratmir@mail.ru>
2019-09-29 16:01:01 +0300

    [clothes] Добавлены модели БД кофт, брюк и обуви.

---
+++ packages/clothes/db/ClothesShoe.js
@@ -0,0 +1,40 @@
+let farms = call('farms');
+
+module.exports = (sequelize, DataTypes) => {
+    const model = sequelize.define("ClothesShoe", {
+        id: {
+            type: DataTypes.INTEGER(11),
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING(30),
+            allowNull: false,
+        },
+        variation: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false,
+        },
+        textures: {
+            type: DataTypes.STRING(128),
+            allowNull: false,
+        },
+        sex: {
+            type: DataTypes.TINYINT(1),
+            allowNull: false,
+        },
+        class: {
+            type: DataTypes.INTEGER(11),
+            defaultValue: 1,
+            allowNull: false,
+        }
+    }, {
+        timestamps: false
+    });
+
+    return model;
+};
---
+++ packages/clothes/db/ClothesTop.js
@@ -0,0 +1,48 @@
+let farms = call('farms');
+
+module.exports = (sequelize, DataTypes) => {
+    const model = sequelize.define("ClothesTop", {
+        id: {
+            type: DataTypes.INTEGER(11),
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING(30),
+            allowNull: false,
+        },
+        variation: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false,
+        },
+        pockets: {
+            type: DataTypes.STRING(128),
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.INTEGER(11),
+            allowNull: false,
+        },
+        textures: {
+            type: DataTypes.STRING(128),
+            allowNull: false,
+        },
+        sex: {
+            type: DataTypes.TINYINT(1),
+            allowNull: false,
+        },
+        torso: {
+            type: DataTypes.TINYINT(1),
+            allowNull: false,
+        },
+        class: {
+            type: DataTypes.INTEGER(11),
+            defaultValue: 1,
+            allowNull: false,
+        }
+    }, {
+        timestamps: false
+    });
+
+    return model;
+};
