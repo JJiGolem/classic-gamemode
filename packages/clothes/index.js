5b4b5567aec9b8b0d3d36051c7430749de9aff00
Carter <dj-ratmir@mail.ru>
2019-09-30 04:15:33 +0300

    [clothes] Загрузка новых моделей одежды при иниц. сервера.

--- packages/clothes/index.js
+++ packages/clothes/index.js
@@ -32,6 +32,22 @@ module.exports = {
     },
 
     async init() {
+        var bracelets = await db.Models.ClothesBracelet.findAll();
+        bracelets.forEach(el => {
+            this.list[el.sex]["bracelets"].push(el);
+        });
+        var ears = await db.Models.ClothesEar.findAll();
+        ears.forEach(el => {
+            this.list[el.sex]["ears"].push(el);
+        });
+        var glasses = await db.Models.ClothesGlasses.findAll();
+        glasses.forEach(el => {
+            this.list[el.sex]["glasses"].push(el);
+        });
+        var hats = await db.Models.ClothesHat.findAll();
+        hats.forEach(el => {
+            this.list[el.sex]["hats"].push(el);
+        });
         var pants = await db.Models.ClothesPants.findAll();
         pants.forEach(el => {
             this.list[el.sex]["pants"].push(el);
@@ -40,10 +56,21 @@ module.exports = {
         shoes.forEach(el => {
             this.list[el.sex]["shoes"].push(el);
         });
+        var ties = await db.Models.ClothesTie.findAll();
+        ties.forEach(el => {
+            this.list[el.sex]["ties"].push(el);
+        });
         var tops = await db.Models.ClothesTop.findAll();
         tops.forEach(el => {
             this.list[el.sex]["tops"].push(el);
         });
+        var watches = await db.Models.ClothesWatch.findAll();
+        watches.forEach(el => {
+            this.list[el.sex]["watches"].push(el);
+        });
+
+        var count = bracelets.length + ears.length + glasses.length + hats.length + pants.length + shoes.length + ties.length + tops.length + watches.length;
+        console.log(`[CLOTHES] Одежда загружена (${count} шт.)`);
     },
     getTypes() {
         return Object.keys(this.list[0]);
