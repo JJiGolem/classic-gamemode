0cfdd77ad821817bd27408eba2ae4beb0aa42ef3
Carter <dj-ratmir@mail.ru>
2019-10-02 18:30:59 +0300

    [bins] Выдача рандомного предмета при поиске. Интервал между поиском в одной мусорке - час.

--- packages/bins/events.js
+++ packages/bins/events.js
@@ -1,4 +1,5 @@
 let bins = call('bins');
+let inventory = call('inventory');
 let notifs = call('notifications');
 
 module.exports = {
@@ -6,6 +7,28 @@ module.exports = {
         bins.init();
     },
     "bins.trash.take": (player) => {
-        notifs.info(player, `скоро будет! :)`);
+        var colshape = player.insideBin;
+        if (!colshape) return out(`Вы не возле мусорки`);
+        if (colshape.lastFindTime) {
+            var diff = Date.now() - colshape.lastFindTime;
+            if (diff < bins.findInterval) return notifs.error(player, `Эту мусорку уже вычистили до вас`);
+        }
+
+        var trashInfo = bins.getRandomTrash();
+        var name = inventory.getName(trashInfo.itemId);
+        var out = (text) => {
+            notifs.error(player, text, name)
+        };
+
+        var cant = inventory.cantAdd(player, trashInfo.itemId, {});
+        if (cant) return out(cant);
+
+        inventory.addItem(player, trashInfo.itemId, {}, (e) => {
+            if (e) out(e);
+
+            colshape.lastFindTime = Date.now();
+        });
+
+        notifs.success(player, `Вы нашли ${name}`, `Мусорка`);
     }
 };
--- packages/bins/index.js
+++ packages/bins/index.js
@@ -1,10 +1,56 @@
 "use strict";
 
+let utils = call('utils');
+
 module.exports = {
     // Мусорки
     list: [],
     // Колшейпы мусорок
     colshapes: [],
+    // Интервал между поисками в одной мусорке
+    findInterval: 60 * 60 * 1000,
+    // Инфо о мусоре, который можно найти
+    trashesInfo: [{
+            itemId: 57,
+            price: 10,
+            rarity: 0,
+        },
+        {
+            itemId: 58,
+            price: 10,
+            rarity: 0,
+        },
+        {
+            itemId: 59,
+            price: 10,
+            rarity: 0,
+        },
+        {
+            itemId: 60,
+            price: 10,
+            rarity: 0,
+        },
+        {
+            itemId: 61,
+            price: 10,
+            rarity: 0,
+        },
+        {
+            itemId: 62,
+            price: 10,
+            rarity: 0,
+        },
+        {
+            itemId: 63,
+            price: 10,
+            rarity: 0,
+        },
+        {
+            itemId: 64,
+            price: 10,
+            rarity: 0,
+        },
+    ],
 
     async init() {
         this.list = await db.Models.Bin.findAll();
@@ -14,7 +60,7 @@ module.exports = {
             var colshape = mp.colshapes.newSphere(pos.x, pos.y, pos.z, 1.5);
             colshape.onEnter = (player) => {
                 player.call("bins.inside", [true]);
-                player.insideBin = true;
+                player.insideBin = colshape;
             };
             colshape.onExit = (player) => {
                 player.call("bins.inside", [false]);
@@ -23,5 +69,9 @@ module.exports = {
         });
 
         console.log(`[BINS] Мусорки загружены (${this.list.length} шт.)`);
+    },
+    getRandomTrash() {
+        var rand = utils.randomInteger(0, this.trashesInfo.length - 1);
+        return this.trashesInfo[rand];
     },
 };
