7441a4d32ef3917e6600b6d11f6e129d38fd22b7
Carter <dj-ratmir@mail.ru>
2019-10-02 23:32:08 +0300

    [bins] /binadd. /bindel.

--- packages/bins/index.js
+++ packages/bins/index.js
@@ -3,8 +3,6 @@
 let utils = call('utils');
 
 module.exports = {
-    // Мусорки
-    list: [],
     // Колшейпы мусорок
     colshapes: [],
     // Интервал между поисками в одной мусорке
@@ -59,22 +57,28 @@ module.exports = {
     emptyChance: 15,
 
     async init() {
-        this.list = await db.Models.Bin.findAll();
+        var list = await db.Models.Bin.findAll();
 
-        this.list.forEach(bin => {
-            var pos = new mp.Vector3(bin.x, bin.y, bin.z);
-            var colshape = mp.colshapes.newSphere(pos.x, pos.y, pos.z, 1.5);
-            colshape.onEnter = (player) => {
-                player.call("bins.inside", [true]);
-                player.insideBin = colshape;
-            };
-            colshape.onExit = (player) => {
-                player.call("bins.inside", [false]);
-                delete player.insideBin;
-            };
+        list.forEach(bin => {
+            var colshape = this.createColshape(bin);
+            this.colshapes.push(colshape);
         });
 
-        console.log(`[BINS] Мусорки загружены (${this.list.length} шт.)`);
+        console.log(`[BINS] Мусорки загружены (${list.length} шт.)`);
+    },
+    createColshape(bin) {
+        var pos = new mp.Vector3(bin.x, bin.y, bin.z);
+        var colshape = mp.colshapes.newSphere(pos.x, pos.y, pos.z, 1.5);
+        colshape.onEnter = (player) => {
+            player.call("bins.inside", [true]);
+            player.insideBin = colshape;
+        };
+        colshape.onExit = (player) => {
+            player.call("bins.inside", [false]);
+            delete player.insideBin;
+        };
+        colshape.bin = bin;
+        return colshape;
     },
     getRandomTrash() {
         var interval = this.trashesInfo.map(x => x.chance);
@@ -91,5 +95,35 @@ module.exports = {
         }
 
         return null;
+    },
+    add(pos) {
+        var bin = db.Models.Bin.build({
+            x: pos.x,
+            y: pos.y,
+            z: pos.z
+        });
+        this.colshapes.push(this.createColshape(bin));
+        bin.save();
+    },
+    remove(id) {
+        var i = this.colshapes.findIndex(x => x.bin.id == id);
+        if (i == -1) return;
+
+        this.colshapes[i].bin.destroy();
+        this.colshapes[i].destroy();
+        this.colshapes.splice(i, 1);
+    },
+    getNear(player, range = 2) {
+        var nearBin;
+        var minDist = 99999;
+        this.colshapes.forEach((el) => {
+            var pos = new mp.Vector3(el.bin.x, el.bin.y, el.bin.z);
+            var distance = player.dist(pos);
+            if (distance < minDist && distance < range) {
+                nearBin = el;
+                minDist = distance;
+            }
+        });
+        return nearBin;
     },
 };
