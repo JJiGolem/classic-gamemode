c0b7ebdfa7e81bae419dcfa514dd578015c256d6
Kirill Savchenko <kirsav10@gmail.com>
2019-10-05 11:52:09 +0400

    [clothingShop] Отправка одежды на клиент

--- client_packages/clothingShop/index.js
+++ client_packages/clothingShop/index.js
@@ -1,32 +1,87 @@
 let player = mp.players.local;
+let playerIsFrozen = false;
 
-//let controlsDisabled = false;
+let clothesLoaded = 0;
+let clothesList = {
+    "bracelets": [],
+    "ears": [],
+    "glasses": [],
+    "hats": [],
+    "pants": [],
+    "shoes": [],
+    "ties": [],
+    "tops": [],
+    "watches": [],
+}
+
+let clothesInfo = {
+    "bracelets": {
+        prop: 0,
+        menuName: 'Bracelets'
+    },
+    "ears": {
+        prop: 2,
+        menuName: 'Ears'
+    },
+    "glasses": {
+        prop: 1,
+        menuName: 'Glasses'
+    },
+    "hats": {
+        prop: 0,
+        menuName: 'Hats'
+    },
+    "pants": {
+        component: 4,
+        menuName: 'Pants'
+    },
+    "shoes": {
+        component: 6,
+        menuName: 'Shoes'
+    },
+    "ties": {
+        component: 7,
+        menuName: 'Ties'
+    },
+    "tops": {
+        component: 11,
+        menuName: 'Tops'
+    },
+    "watches": {
+        prop: 6,
+        menuName: 'Watches'
+    },
+}
+
 let rotation = {
     left: false,
     right: false
 }
 
 mp.events.add({
-    'clothingShop.enter': (shopData, gender) => {
+    'clothingShop.enter': (shopData) => {
         bindKeys(true);
         setHeaders(shopData.bType);
-        mp.utils.disablePlayerMoving(true);
         mp.events.call('hud.enable', false);
         mp.game.ui.displayRadar(false);
         mp.callCEFR('setOpacityChat', [0.0]);
-        // prices = priceData;
-        // currentGender = gender;
+        mp.console(JSON.stringify(clothesList));
+        mp.chat.debug(clothesList.length);
         mp.utils.cam.create(shopData.camera.x, shopData.camera.y, shopData.camera.z, shopData.pos.x, shopData.pos.y, shopData.pos.z, 42);
-
+        mp.callCEFV('loader.show = false');
         mp.events.call('selectMenu.show', 'clothingMain');
         player.position = new mp.Vector3(shopData.pos.x, shopData.pos.y, shopData.pos.z);
-        player.freezePosition(true);
+        if (!playerIsFrozen) {
+            mp.utils.disablePlayerMoving(true);
+            player.freezePosition(true);
+        }
         setTimeout(() => {
             player.setHeading(shopData.pos.h);
             mp.prompt.show('Используйте <span>A</span> и <span>D</span> для того, чтобы вращать персонажа');
         }, 100);
     },
     'clothingShop.exit': () => {
+        playerIsFrozen = false;
         mp.events.call(`selectMenu.hide`);
         bindKeys(false);
         mp.utils.cam.destroy();
@@ -41,6 +96,22 @@ mp.events.add({
     'render': () => {
         if (rotation.left) player.setHeading(player.getHeading() - 2);
         if (rotation.right) player.setHeading(player.getHeading() + 2);
+    },
+    'clothingShop.list.get': (key, list) => {
+        clothesList[key] = list;
+        mp.chat.debug(`${key} get. Length ${list.length}`);
+        clothesLoaded++;
+        if (clothesLoaded == 9) {
+            mp.chat.debug(`data received`);
+            clothesLoaded = 0;
+            mp.events.callRemote('clothingShop.enter');
+        }
+    },
+    'clothingShop.player.freeze': () => {
+        mp.callCEFV('loader.show = true');
+        mp.utils.disablePlayerMoving(true);
+        player.freezePosition(true);
+        playerIsFrozen = true;
     }
 });
 
@@ -94,6 +165,9 @@ function setHeaders(type) {
             img = 'ponsonbys';
             break;
     }
-   // mp.callCEFV(`selectMenu.menus["clothingMain"].headerImg = '${img}.png'`)
     ['Main'].forEach(name => mp.callCEFV(`selectMenu.menus["clothing${name}"].headerImg = '${img}.png'`));
+}
+
+function initClothingShopMenu() {
+
 }
--- packages/clothingShop/events.js
+++ packages/clothingShop/events.js
@@ -1,8 +1,7 @@
 let clothingShop = require('./index.js');
 let money = call('money');
 let inventory = call('inventory');
-let phone = call('phone');
-
+let clothes = call('clothes');
 module.exports = {
     "init": () => {
         clothingShop.init();
@@ -10,19 +9,31 @@ module.exports = {
     "playerEnterColshape": (player, shape) => {
         if (!player.character) return;
         if (shape.isClothingShop) {
-            let id = shape.clothingShopId;
             player.call('chat.message.push', [`!{#ffffff}[debug]${player.name} зашел в колшейп CS ${shape.clothingShopId}`]);
-            let data = clothingShop.getRawShopData(id);
-            // let priceConfig = supermarket.getPriceConfig();
-            player.call('clothingShop.enter', [data]);
             player.currentClothingShopId = shape.clothingShopId;
+            if (player.hasValidClothesData) {
+                mp.events.call('clothingShop.enter', player);
+            } else {
+                player.call('clothingShop.player.freeze');
+                let gender = player.character.gender ? '0' : '1';
+                console.log(gender);
+                let list = clothes.getClientList()[gender];
+                for (let key in list) {
+                    player.call('clothingShop.list.get', [key, list[key]]);
+                }
+                player.hasValidClothesData = true;
+            }
         }
     },
     "playerExitColshape": (player, shape) => {
         if (!player.character) return;
         if (shape.isClothingShop) {
             player.call('chat.message.push', [`!{#ffffff}[debug]${player.name} вышел с колшейпа CS ${shape.clothingShopId}`]);
-            //player.call('supermarket.exit');
         }
+    },
+    "clothingShop.enter": (player) => {
+        let id = player.currentClothingShopId;
+        let data = clothingShop.getRawShopData(id);
+        player.call('clothingShop.enter', [data]);
     },
 };
