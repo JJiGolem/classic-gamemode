1d13aa5f9cf439c42bd6a54102ffb12e9261e2dc
deusmemes <maxin29999@gmail.com>
2019-10-07 20:53:36 +0400

    [Phone] load messages to dialog & not read message

--- react/src/modules/phone/components/DialogPage.jsx
+++ react/src/modules/phone/components/DialogPage.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import React, {Component, Fragment} from 'react';
 import { connect } from 'react-redux';
 
@@ -17,11 +18,13 @@ class DialogPage extends Component {
         super(props);
         this.state = {
             inputMessage: '',
-            isShowLoad: true
+            isLoad: false,
+            isMyMessage: true
         };
 
         this.handleChangeMessage = this.handleChangeMessage.bind(this);
         this.loadMessages = this.loadMessages.bind(this);
+        this.getNotReadMessages = this.getNotReadMessages.bind(this);
     }
 
     componentDidMount() {
@@ -31,13 +34,22 @@ class DialogPage extends Component {
         objDiv.scrollTop = objDiv.scrollHeight;
 
         readDialog(dialog.number);
+
+        // setInterval(() => {
+        //     this.props.addMessage('hi', Date.now(), dialog.number, false, false);
+        // }, 1000);
     }
 
     componentDidUpdate() {
-        const { isShowLoad } = this.state;
+        const { isLoad, isMyMessage } = this.state;
+        const { readDialog, dialog } = this.props;
         const objDiv = this.refList;
 
-        objDiv.scrollTop = objDiv.scrollHeight;
+        if (!isLoad && isMyMessage) {
+            objDiv.scrollTop = objDiv.scrollHeight;
+            this.setState({ isMyMessage: false });
+            readDialog(dialog.number);
+        }
     }
 
     componentWillUnmount() {
@@ -61,8 +73,7 @@ class DialogPage extends Component {
             addMessage(inputMessage, Date.now(), dialog.number, true, true);
             // eslint-disable-next-line no-undef
             mp.trigger('phone.message.send', inputMessage, dialog.number);
-            this.setState({ inputMessage: '' });
-            // this.refList.scrollTop = this.refList.scrollHeight + 300;
+            this.setState({ inputMessage: '', isLoad: false, isMyMessage: true });
         }
     }
 
@@ -78,6 +89,9 @@ class DialogPage extends Component {
 
     loadMessages() {
         const { loadMessages, dialog } = this.props;
+
+        this.setState({ isLoad: true });
+        mp.trigger('phone.messages.load', dialog.number);
 
         // loadMessages(dialog.number, [
         //     {
@@ -100,14 +114,13 @@ class DialogPage extends Component {
 
     getMessages(number) {
         const { dialogs } = this.props;
-        const { isShowLoad } = this.state;
 
         let messages = dialogs.find(d => d.number === number).PhoneMessages;
 
         if (messages.length !== 0) {
             return (
                 <Fragment>
-                    { isShowLoad && <div className="messages_load_phone-react" onClick={this.loadMessages}>Загрузить еще</div> }
+                    {messages.length > 5 && <div className="messages_load_phone-react" onClick={this.loadMessages}>Загрузить еще</div>}
                     { messages.map((message, index) => (
                             <div>
                                 {
@@ -158,6 +171,22 @@ class DialogPage extends Component {
         closeApp();
     }
 
+    getNotReadMessages() {
+        const { dialog, readDialog } = this.props;
+
+        return (
+            <div 
+                className="not_read_phone-react"
+                onClick={() => {
+                readDialog(dialog.number);
+                this.setState({ isLoad: false });
+                this.refList.scrollTop = this.refList.scrollHeight;
+            }}>
+                <span>{ dialog.PhoneMessages.filter(message => !message.isRead).length }</span>
+            </div>
+        )
+    }
+
     render() {
 
         const { dialog } = this.props;
@@ -181,25 +210,36 @@ class DialogPage extends Component {
 
                     
 
-                    <div className='messages_list-phone-react' ref={(list) => {this.refList = list}}>
+                    <div className='messages_list-phone-react' ref={(list) => {this.refList = list}}
+                        onScroll={() => { 
+                            if (this.refList.offsetHeight + this.refList.scrollTop >= this.refList.scrollHeight) {
+                                this.props.readDialog(dialog.number)
+                            }
+                         }}
+                    >
                         {this.getMessages(dialog.number)}
                     </div>
 
-                        <div>
-                            <textarea className='input_message-phone-react' onKeyDown={this.keyDown.bind(this)}
-                                  type='text' onChange={this.handleChangeMessage}
-                                  value={this.state.inputMessage}
-                                  placeholder='Написать...'
-                            />
-                            <button onClick={this.sendMessage.bind(this)} id='button_send_message-phone-react'>
-                                <svg xmlns="http://www.w3.org/2000/svg" className='ico_but_send_message' viewBox="0 0 31 40.762">
-                                    <g id="Group_1" data-name="Group 1" transform="translate(6.824 7.73)">
-                                        <path id="Fill_162" data-name="Fill 162" d="M29.5,0H1.5a1.5,1.5,0,0,0,0,3h28a1.5,1.5,0,0,0,0-3" transform="translate(-6.824 30.032)" fill="#1a1919"/>
-                                        <path id="Fill_163" data-name="Fill 163" d="M2.56,15.186,12.6,5.152V32.229a1.5,1.5,0,0,0,3,0V5.139l9.83,10.016a1.5,1.5,0,0,0,2.141-2.1L15.2.449a1.5,1.5,0,0,0-2.132-.01L.439,13.064A1.5,1.5,0,0,0,2.56,15.186" transform="translate(-5.412 -7.73)" fill="#1a1919"/>
-                                    </g>
-                                </svg>
-                            </button>
-                        </div>
+                    { dialog.PhoneMessages.some(message => !message.isRead) 
+                        && this.refList
+                        && this.refList.scrollTop !== this.refList.scrollHeight 
+                        && this.getNotReadMessages() }
+
+                    <div>
+                        <textarea className='input_message-phone-react' onKeyDown={this.keyDown.bind(this)}
+                                type='text' onChange={this.handleChangeMessage}
+                                value={this.state.inputMessage}
+                                placeholder='Написать...'
+                        />
+                        <button onClick={this.sendMessage.bind(this)} id='button_send_message-phone-react'>
+                            <svg xmlns="http://www.w3.org/2000/svg" className='ico_but_send_message' viewBox="0 0 31 40.762">
+                                <g id="Group_1" data-name="Group 1" transform="translate(6.824 7.73)">
+                                    <path id="Fill_162" data-name="Fill 162" d="M29.5,0H1.5a1.5,1.5,0,0,0,0,3h28a1.5,1.5,0,0,0,0-3" transform="translate(-6.824 30.032)" fill="#1a1919"/>
+                                    <path id="Fill_163" data-name="Fill 163" d="M2.56,15.186,12.6,5.152V32.229a1.5,1.5,0,0,0,3,0V5.139l9.83,10.016a1.5,1.5,0,0,0,2.141-2.1L15.2.449a1.5,1.5,0,0,0-2.132-.01L.439,13.064A1.5,1.5,0,0,0,2.56,15.186" transform="translate(-5.412 -7.73)" fill="#1a1919"/>
+                                </g>
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </Fragment>
         );
