c23ad9c69c3355019bc3888f584a0c5b3c4ffc46
Данил Аникьев <anikyev95@mail.ru>
2019-10-08 20:04:27 +0400

    [biz] sell to gov fix

--- packages/bizes/index.js
+++ packages/bizes/index.js
@@ -67,7 +67,7 @@ let dropBiz = function(biz, sellToGov = false) {
             } else {
                 console.log("[bizes] Biz dropped " + biz.info.id + ". But player didn't getmoney");
             }
-        }, sellToGov ? `Продажа бизнеса #${info.id} государству` : `Слет бизнеса #${info.id}`);
+        }, sellToGov ? `Продажа бизнеса #${biz.info.id} государству` : `Слет бизнеса #${biz.info.id}`);
     });
 };
 let setTimer = function(biz) {
