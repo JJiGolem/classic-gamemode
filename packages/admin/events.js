6096e94255fac742f536eecbb78476d18bf936c6
Kirill Savchenko <kirsav10@gmail.com>
2019-10-14 01:54:35 +0400

    [admin] Теперь обработчик команд обрабатывает тип аргумента.

--- packages/admin/events.js
+++ packages/admin/events.js
@@ -17,10 +17,20 @@ module.exports = {
         // TODO: проверка на access
         if (player.character.admin >= cmd.access) {
             if (cmd.args) {
-                let requiredArgs = cmd.args.split('] ').length;
-
-                if (args.length < requiredArgs) {
+                let requiredArgs;
+                if (admin.isTerminalCommand(cmd.args)) {
+                    requiredArgs = cmd.args.split(' ');
+                } else {
+                    requiredArgs = cmd.args.split('] ');
+                }
+                if (args.length < requiredArgs.length) {
                     return player.call('chat.message.push', [`!{#ffffff} Используйте: ${command} ${cmd.args}`]);
+                }
+                for (let i = 0; i < requiredArgs.length; i++) {
+                    let argType = requiredArgs[i].split(":")[1];
+                    if (!argType) continue;
+                    if (!admin.isValidArg(argType, args[i])) return player.call('chat.message.push', [`Неверное значение "${args[i]}" для параметра ${requiredArgs[i]}`])
+                    else args[i] = admin.toValidArg(argType, args[i]);
                 }
             }
 
@@ -139,7 +149,7 @@ module.exports = {
     // Поступила жалоба от игрока
     "admin.report": (player, message) => {
         var media = player.character.Promocode.media;
-        var color = (!media)? "#ffe838" : "#ff3ec8";
+        var color = (!media) ? "#ffe838" : "#ff3ec8";
         player.call('chat.message.push', [`!{#87c924}Ваш репорт:!{${color}} ${message}`]);
         mp.events.call("admin.notify.all", `!{#87c924}${player.name}[${player.id}]:!{${color}} ${message}`);
     },
