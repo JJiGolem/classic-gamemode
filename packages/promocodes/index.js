fe45afbda07d056a62d4c991085a51b2c88a1d3a
Carter <dj-ratmir@mail.ru>
2019-10-14 17:55:41 +0300

    [promocodes] Обработчик выдачи наличных у подарочного промокода.

--- packages/promocodes/index.js
+++ packages/promocodes/index.js
@@ -18,9 +18,12 @@ module.exports = {
     codesCount: 100,
     // Кол-во минут на сервере для получения награды за промокод
     completedMinutes: 10 * 60,
+    // Подарочные промокоды
+    giftPromocodes: [],
 
     init() {
         this.fillCodes();
+        this.loadGiftPromocodesFromDB();
     },
     async fillCodes() {
         var codes = [];
@@ -69,7 +72,12 @@ module.exports = {
         };
         if (player.character.Promocode.promocode == code) return out(`Нельзя активировать свой промокод`);
 
-        // TODO: проверка на подарочные промокоды
+        // подарочный промкод
+        var giftPromocode = this.giftPromocodes.find(x => x.promocode == code);
+        if (giftPromocode) {
+            this.activateGift(player, giftPromocode);
+            return;
+        }
 
         var promocode = await db.Models.Promocode.findOne({
             where: {
@@ -96,6 +104,23 @@ module.exports = {
 
         mp.events.call("player.promocode.activated", player, promocode);
         notifs.success(player, `Промокод активирован`, header);
+    },
+    activateGift(player, giftPromocode) {
+        switch (giftPromocode.rewardId) {
+            case 1: // выдача наличных
+                var count = giftPromocode.data.count;
+                money.addCash(player, count, (res) => {
+                    if (!res) return notifs.error(player, `Ошибка начисления наличных`);
+
+                    var i = this.giftPromocodes.indexOf(giftPromocode);
+                    if (i != -1) this.giftPromocodes.splice(i, 1);
+                    giftPromocode.destroy();
+                    notifs.success(player, `Подарочный промокод на наличные средства активирован`);
+                }, `Подарочный промокод ${giftPromocode.promocode} на выдачу наличных`);
+                break;
+            default:
+                return notifs.error(player, `Неизвестный тип подарка`);
+        }
     },
     async check(player) {
         var minutes = parseInt((Date.now() - player.authTime) / 1000 / 60 % 60) + player.character.minutes;
@@ -140,5 +165,10 @@ module.exports = {
             }, `Награда за приглашенного игрока ${player.name}`);
         }, `Награда за выполнение условий промокода ${(typeof inviter == 'number')? inviter : inviter.name}`);
 
+    },
+    async loadGiftPromocodesFromDB() {
+        this.giftPromocodes = await db.Models.GiftPromocode.findAll();
+
+        console.log(`[PROMOCODES] Подарочные промокоды загружены (${this.giftPromocodes.length} шт.)`);
     },
 };
