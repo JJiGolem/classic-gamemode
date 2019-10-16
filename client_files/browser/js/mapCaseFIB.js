176e05986b12ddb4be6608e3e4d9ef39a023c2b6
carter <dj-ratmir@mail.ru>
2019-10-16 21:30:10 +0300

    [mapCase] Фикс поиска преступника на картею

--- client_files/browser/js/mapCaseFIB.js
+++ client_files/browser/js/mapCaseFIB.js
@@ -149,7 +149,7 @@ Vue.component('map-case-fib-profile', {
     methods: {
         showOnMap() {
             // TODO: Показать претупника на карте.
-            mp.trigger(`callRemote`, `mapCase.pd.wanted.search`, this.profileData.dbSearch.id);
+            mp.trigger(`callRemote`, `mapCase.pd.wanted.search`, this.profileData[this.currentMenuFocus].id);
         },
         showOverWindow(winName) {
             mapCase.currentOverWindow = `map-case-${mapCase.type}-over-${winName}`;
