a865c4eb36fca8138618ab6707c752f17f4a388c
Данил Аникьев <anikyev95@mail.ru>
2019-10-19 12:01:48 +0400

    [utils.camera] Удаление логов

--- client_files/utils/camera.js
+++ client_files/utils/camera.js
@@ -9,9 +9,6 @@ exports = {
     /// Создание камеры
     /// fov can be null
     create(x, y, z, toX, toY, toZ, fov) {
-        mp.console("create");
-        mp.console(JSON.stringify({x: x, y: y, z: z, toX: toX, toY: toY, toZ: toZ, fov: fov}));
-
         selectedCam = 0;
         cams[selectedCam].setCoord(x, y, z);
         cams[selectedCam].pointAtCoord(toX, toY, toZ);
@@ -21,8 +18,6 @@ exports = {
     },
     /// Удаление камеры
     destroy() {
-        mp.console("destroy");
-
         selectedCam = null;
         cams[0].setActive(false);
         cams[1].setActive(false);
@@ -32,10 +27,6 @@ exports = {
     /// fov can be null
     moveTo(x, y, z, toX, toY, toZ, time, fov) {
         if (selectedCam == null) return;
-
-        mp.console("moveTo");
-        mp.console(JSON.stringify({x: x, y: y, z: z, toX: toX, toY: toY, toZ: toZ, time: time, fov: fov}));
-
         let newSelected = selectedCam == 0 ? 1 : 0;
         cams[newSelected].setCoord(x, y, z);
         cams[newSelected].pointAtCoord(toX, toY, toZ);
@@ -46,10 +37,6 @@ exports = {
     /// Телепорт камеры
     tpTo(x, y, z, toX, toY, toZ, fov) {
         if (selectedCam == null) return;
-
-        mp.console("tpTo");
-        mp.console(JSON.stringify({x: x, y: y, z: z, toX: toX, toY: toY, toZ: toZ, fov: fov}));
-
         cams[selectedCam].setCoord(x, y, z);
         cams[selectedCam].pointAtCoord(toX, toY, toZ);
         if (fov) cams[selectedCam].setFov(fov);
