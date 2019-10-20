21000d9801bdff05bd3569cdd02572192515a533
Данил Аникьев <anikyev95@mail.ru>
2019-10-21 01:00:32 +0400

    [auth] whitelist fix

--- packages/whitelist/events.js
+++ packages/whitelist/events.js
@@ -8,15 +8,15 @@ module.exports = {
         await whitelist.init();
         inited(__dirname);
     },
-    "player.joined": (player) => {
-        if (!whitelist.isEnabled()) return;
+    // "player.joined": (player) => {
+    //     if (!whitelist.isEnabled()) return;
 
-        if (whitelist.isInWhiteList(player.socialClub)) {
-            console.log(`[WHITELIST] ${player.socialClub} зашел на сервер по вайтлисту`);
-        } else {
-            console.log(`[WHITELIST] ${player.socialClub} пытался войти, но его нет в вайтлисте`);
-            player.call('notifications.push.error', [`Social Club ${player.socialClub} не находится в вайтлисте`]);
-            player.kick("Kicked");
-        }
-    }
+    //     if (whitelist.isInWhiteList(player.socialClub)) {
+    //         console.log(`[WHITELIST] ${player.socialClub} зашел на сервер по вайтлисту`);
+    //     } else {
+    //         console.log(`[WHITELIST] ${player.socialClub} пытался войти, но его нет в вайтлисте`);
+    //         player.call('notifications.push.error', [`Social Club ${player.socialClub} не находится в вайтлисте`]);
+    //         player.kick("Kicked");
+    //     }
+    // }
 }
