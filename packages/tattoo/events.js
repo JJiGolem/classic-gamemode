69908828685cbb7e590728eaf445d3b9e9984d23
Kirill Savchenko <kirsav10@gmail.com>
2019-10-27 01:35:07 +0400

    [tattoo] Модель тату-салона. Загрузка конфига татуировок на клиент. Вход в тату-салон. Функции для взаимодействия с бизнесом.

--- packages/tattoo/events.js
+++ packages/tattoo/events.js
@@ -8,5 +8,40 @@ module.exports = {
     "characterInit.done": async (player) => {
         await tattoo.loadCharacterTattoos(player);
         tattoo.setCharacterTattoos(player)
-    }
+    },
+    "playerEnterColshape": (player, shape) => {
+        
+        if (!player.character) return;
+        if (shape.isTattooParlor) {
+            player.currentTattooParlorId = shape.tattooParlorId;
+            player.dimension = player.id + 1;
+            if (player.hasValidTattooData) {
+                mp.events.call('tattoo.enter', player);
+            } else {
+                player.call('tattoo.player.freeze');
+                
+                let tattooList = tattoo.getRawTattooList();
+                let packsCount = tattooList.length % 100 == 0 ? 
+                parseInt(tattooList.length / 100) : parseInt(tattooList.length / 100) + 1;
+                console.log(packsCount)
+                while (tattooList.length > 0) {
+                    let pack = tattooList.slice(0, 100);
+                    console.log(pack.length);
+                    tattooList.splice(0, 100);
+                    player.call('tattoo.pack.get', [pack, packsCount]);
+                }
+                player.hasValidTattooData = true;
+            }
+        }
+    },
+    "tattoo.enter": (player) => {
+        let id = player.currentTattooParlorId;
+        let data = tattoo.getRawShopData(id);
+        let gender = player.character.gender ? 0 : 1;
+        player.call('tattoo.enter', [data, gender]);
+    },
+    "tattoo.exit": (player) => {
+        player.dimension = 0;
+        inventory.updateAllView(player);
+    },
 }
--- packages/tattoo/index.js
+++ packages/tattoo/index.js
@@ -1,15 +1,28 @@
 const fs = require('fs');
+let bizes;
 
 let parseData = {};
+let tattooList;
 
 module.exports = {
-    init() {
-        console.log('tattoos init');
+    business: {
+        type: 8,
+        name: "Тату-салон",
+        productName: "Материалы",
+    },
+    rentPerDayMultiplier: 0.01,
+    minPriceMultiplier: 1.0,
+    maxPriceMultiplier: 2.0,
+    productPrice: 20,
+    async init() {
+        bizes = call('bizes');
+        await this.loadShopsFromDB();
+        await this.loadTattoosFromDB();
+        this.getRawTattooList();
     },
     parseTattoos() {
         fs.readdirSync("packages/tattoo/data/").forEach(file => {
             let collection = file.slice(0, file.length - 5);
-            console.log(collection);
             let data = require(`./data/${file}`);
             data.forEach((current) => {
                 db.Models.Tattoo.create({
@@ -31,7 +44,6 @@ module.exports = {
     },
     setCharacterTattoos(player) {
         let tattoos = player.character.tattoos;
-        console.log(tattoos);
         tattoos.forEach((current) => {
             player.setDecoration(mp.joaat(current.collection), mp.joaat(current.hashName));
         });
@@ -57,5 +69,96 @@ module.exports = {
         tattoos.splice(index, 1);
         player.clearDecorations();
         this.setCharacterTattoos(player);
+    },
+    async loadTattoosFromDB() {
+        tattooList = await db.Models.Tattoo.findAll();
+        console.log(`[TATTOO] Загружено татуировок: ${tattooList.length}`);
+    },
+    async loadShopsFromDB() {
+        shops = await db.Models.TattooParlor.findAll();
+        for (var i = 0; i < shops.length; i++) {
+            this.createShop(shops[i]);
+        }
+        console.log(`[TATTOO] Загружено тату-салонов: ${i}`);
+    },
+    createShop(shop) {
+        mp.blips.new(75, new mp.Vector3(shop.x, shop.y, shop.z),
+            {
+                name: 'Тату-салон',
+                color: 0,
+                shortRange: true,
+            });
+
+        mp.markers.new(1, new mp.Vector3(shop.x, shop.y, shop.z - 0.05), 0.8,
+            {
+                color: [255, 104, 59, 200],
+                visible: true,
+                dimension: 0
+            });
+
+        let shape = mp.colshapes.newSphere(shop.x, shop.y, shop.z, 1.8);
+        shape.isTattooParlor = true;
+        shape.tattooParlorId = shop.id;
+    },
+    getRawShopData(id) {
+        let shop = shops.find(x => x.id == id);
+        return {
+            pos: {
+                x: shop.placeX,
+                y: shop.placeY,
+                z: shop.placeZ,
+                h: shop.placeH
+            },
+            camera: {
+                x: shop.cameraX,
+                y: shop.cameraY,
+                z: shop.cameraZ
+            },
+            bType: shop.bType,
+            priceMultiplier: shop.priceMultiplier
+        }
+    },
+    getBizParamsById(id) {
+        let shop = shops.find(x => x.bizId == id);
+        if (!shop) return;
+        let params = [
+            {
+                key: 'priceMultiplier',
+                name: 'Наценка на услуги',
+                max: this.maxPriceMultiplier,
+                min: this.minPriceMultiplier,
+                current: shop.priceMultiplier
+            }
+        ]
+        return params;
+    },
+    setBizParam(id, key, value) {
+        let shop = shops.find(x => x.bizId == id);
+        if (!shop) return;
+        shop[key] = value;
+        shop.save();
+    },
+    getProductsAmount(id) {
+        let shop = shops.find(x => x.id == id);
+        let bizId = shop.bizId;
+        let products = bizes.getProductsAmount(bizId);
+        return products;
+    },
+    removeProducts(id, products) {
+        let shop = shops.find(x => x.id == id);
+        let bizId = shop.bizId;
+        bizes.removeProducts(bizId, products);
+    },
+    getPriceMultiplier(id) {
+        let shop = shops.find(x => x.id == id);
+        return shop.priceMultiplier;
+    },
+    updateCashbox(id, money) {
+        let shop = shops.find(x => x.id == id);
+        let bizId = shop.bizId;
+        bizes.bizUpdateCashBox(bizId, money);
+    },
+    getRawTattooList() {
+        return tattooList.map(current => current.dataValues);
     }
 }
