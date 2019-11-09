1d4931919f3d58a0d150aa5b69585f6c78b05038
Carter <dj-ratmir@mail.ru>
2019-11-09 21:41:22 +0300

    [police] Запрет ударов/стрельбы в наручниках. Запрет стрельбы при следовании.

--- client_files/utils/index.js
+++ client_files/utils/index.js
@@ -424,6 +424,7 @@ mp.events.add('render', () => {
         mp.game.controls.disableControlAction(0, 31, true); /// вперед назад
         mp.game.controls.disableControlAction(0, 30, true); /// влево вправо
         mp.game.controls.disableControlAction(0, 24, true); /// удары
+        mp.game.controls.disableControlAction(0, 257, true); /// стрельба
         mp.game.controls.disableControlAction(1, 200, true); // esc
         mp.game.controls.disableControlAction(0, 140, true); /// удары R
         mp.game.controls.disableControlAction(24, 37, true); /// Tab
