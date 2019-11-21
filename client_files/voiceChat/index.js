7f61a87906752ae2e215cb2a512de2634094ad11
Данил Аникьев <anikyev95@mail.ru>
2019-11-22 02:11:56 +0400

    [voiceChat] дебаг

--- client_files/voiceChat/index.js
+++ client_files/voiceChat/index.js
@@ -36,14 +36,13 @@ let channels = {};
 /// autoConnection будет ли автоматически подключаться/отключаться
 mp.speechChanel.addChannel = (name, maxRange = 0, autoConnection = false, use3d = false) => {
     channels[name] = {"maxRange": maxRange, "autoConnection": autoConnection, "use3d": use3d};
-}
+};
 
 /// Подключить выбранного игрока к каналу связи
 mp.speechChanel.connect = (player, channel) => {
     if (player == null) return;
-    mp.console("speechChanel.connect");
-    let index = listeners.findIndex( x => x.playerId == player.remoteId);
-    if (index != -1) {
+    let index = listeners.findIndex( x => x.playerId === player.remoteId);
+    if (index !== -1) {
         if (!listeners[index].channels.includes(channel)) {
             listeners[index].channels.push(channel);
             updateCurrent(player, index, channel);
@@ -56,25 +55,25 @@ mp.speechChanel.connect = (player, channel) => {
     }
 
     player.voiceVolume = 1.0;
-}
+};
 mp.events.add("voiceChat.connect", (playerId, channel) => {
+    mp.console(`speechChanel.connect ${channel}`);
     mp.speechChanel.connect(mp.players.atRemoteId(playerId), channel);
 });
 
 /// Отключить выбранного игрока от канала связи
 mp.speechChanel.disconnect = (player, channel, isSend = false) => {
     if (player == null) return;
-    mp.console("speechChanel.disconnect");
-    let index = listeners.findIndex( x => x.playerId == player.remoteId);
-    if (index == -1) return;
+    let index = listeners.findIndex( x => x.playerId === player.remoteId);
+    if (index === -1) return;
     if (channel == null) {
         listeners.splice(index, 1);
     }
     else {
-        let channelIndex = listeners[index].channels.findIndex(x => x == channel);
-        channelIndex != -1 && listeners[index].channels.splice(channelIndex, 1);
+        let channelIndex = listeners[index].channels.findIndex(x => x === channel);
+        channelIndex !== -1 && listeners[index].channels.splice(channelIndex, 1);
 
-        if (listeners[index].channels.length == 0) {
+        if (listeners[index].channels.length === 0) {
             listeners.splice(index, 1);
             mp.events.callRemote("voiceChat.remove", player);
         }
@@ -85,22 +84,22 @@ mp.speechChanel.disconnect = (player, channel, isSend = false) => {
     if (channel == null && isSend) {
         mp.events.callRemote("voiceChat.remove", player);
     }
-}
+};
 mp.events.add("voiceChat.disconnect", (playerId, channel) => {
     mp.speechChanel.disconnect(mp.players.atRemoteId(playerId), channel);
 });
 
 let updateCurrent = function(player, index, newCh) {
-    if (channels[listeners[index].current].maxRange == 0) return;
+    if (channels[listeners[index].current].maxRange === 0) return;
     let maxChannel = listeners[index].current;
     if (newCh) {
-        if (channels[newCh].maxRange == 0 || channels[newCh].maxRange > channels[listeners[index].current].maxRange) {
+        if (channels[newCh].maxRange === 0 || channels[newCh].maxRange > channels[listeners[index].current].maxRange) {
             maxChannel = newCh;
         }
     }
     else {
         for (let i = 0, max = -1; i < listeners[index].channels.length; i++) {
-            if (channels[listeners[index].channels[i]].maxRange == 0) {
+            if (channels[listeners[index].channels[i]].maxRange === 0) {
                 maxChannel = listeners[index].channels[i];
                 break;
             }
@@ -110,9 +109,10 @@ let updateCurrent = function(player, index, newCh) {
             }
         }
     }
+    mp.console(`updateCurrent ${maxChannel}`);
     listeners[index].current = maxChannel;
     player.voice3d = channels[maxChannel].use3d;
-}
+};
 
 
 mp.speechChanel.addChannel("voice", 20, true, true);
@@ -120,7 +120,7 @@ mp.speechChanel.addChannel("voice", 20, true, true);
 mp.timer.addInterval(() => {
     /// Автоматическое подключение к заданным каналам всех игроков в зоне стрима
 	mp.players.forEachInStreamRange(player => {
-		if (player != mp.players.local && mp.players.local.dimension == player.dimension) {
+		if (player != mp.players.local && mp.players.local.dimension === player.dimension) {
             let dist = mp.game.system.vdist(player.position.x, player.position.y, player.position.z,
                 mp.players.local.position.x,  mp.players.local.position.y,  mp.players.local.position.z);
             for (let key in channels) {
@@ -136,11 +136,12 @@ mp.timer.addInterval(() => {
         let player = mp.players.atRemoteId(listeners[i].playerId);
         if (player == null) return;
 		if (player.handle !== 0) {
-            if (channels[listeners[i].current].maxRange != 0) {
+            if (channels[listeners[i].current].maxRange !== 0) {
                 let dist = mp.game.system.vdist(player.position.x, player.position.y, player.position.z,
                     mp.players.local.position.x,  mp.players.local.position.y,  mp.players.local.position.z);
 
-                if(dist > channels[listeners[i].current].maxRange || player.dimension != mp.players.local.dimension) {
+                if(dist > channels[listeners[i].current].maxRange || player.dimension !== mp.players.local.dimension) {
+                    mp.console(`speechChanel.disconnect ${listeners[i].current}`);
                     mp.speechChanel.disconnect(player, listeners[i].current);
                     i--;
                 }
@@ -160,14 +161,14 @@ mp.timer.addInterval(() => {
 
 
 mp.events.add("playerQuit", (player) => {
-    if (player.remoteId != mp.players.local.remoteId) {
+    if (player.remoteId !== mp.players.local.remoteId) {
         mp.speechChanel.disconnect(player, null);
     }
 });
 
 mp.events.add("playerDeath", (player) => {
-    if (player.remoteId == mp.players.local.remoteId) {
-        while (listeners.length != 0) {
+    if (player.remoteId === mp.players.local.remoteId) {
+        while (listeners.length !== 0) {
             mp.events.callRemote("voiceChat.remove", mp.players.atRemoteId(listeners[0].playerId));
             listeners.splice(0, 1);
         }
