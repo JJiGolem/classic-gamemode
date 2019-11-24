a06f83235d0d41ecb77f0fa90fc391140d8ac83d
Carter <dj-ratmir@mail.ru>
2019-11-24 17:28:25 +0300

    [promocodes] Промокод PayDay выключен.

--- packages/promocodes/events.js
+++ packages/promocodes/events.js
@@ -1,11 +1,11 @@
 let promocodes = call("promocodes");
 
 // TODO: Временное решение
-let tempPayDays = [];
+// let tempPayDays = [];
 
 module.exports = {
     "init": async () => {
-        tempPayDays = await db.Models.TempPayDay.findAll();
+        // tempPayDays = await db.Models.TempPayDay.findAll();
         await promocodes.init();
         inited(__dirname);
     },
@@ -14,21 +14,21 @@ module.exports = {
     },
     "promocodes.activate": (player, code) => {
         // TODO: Временное решение
-        if (code.toLowerCase() == 'payday') {
-            var notifs = call("notifications");
-            if (tempPayDays.find(x => x.characterId == player.character.id)) return notifs.error(player, `Награда уже получена`, `Промокод PayDay`);
-            call("money").addCash(player, 10000, (res) => {
-                if (!res) return notifs.error(player, `Ошибка начисления наличных`);;
-
-                notifs.success(player, `Получена награда!`, `Промокод PayDay`);
-                var tempPayDay = db.Models.TempPayDay.build({
-                    characterId: player.character.id
-                });
-                tempPayDays.push(tempPayDay);
-                tempPayDay.save();
-            }, `Использование промокода PayDay`);
-            return;
-        }
+        // if (code.toLowerCase() == 'payday') {
+        //     var notifs = call("notifications");
+        //     if (tempPayDays.find(x => x.characterId == player.character.id)) return notifs.error(player, `Награда уже получена`, `Промокод PayDay`);
+        //     call("money").addCash(player, 10000, (res) => {
+        //         if (!res) return notifs.error(player, `Ошибка начисления наличных`);;
+        //
+        //         notifs.success(player, `Получена награда!`, `Промокод PayDay`);
+        //         var tempPayDay = db.Models.TempPayDay.build({
+        //             characterId: player.character.id
+        //         });
+        //         tempPayDays.push(tempPayDay);
+        //         tempPayDay.save();
+        //     }, `Использование промокода PayDay`);
+        //     return;
+        // }
         promocodes.activate(player, code);
     },
 };
