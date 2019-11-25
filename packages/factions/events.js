a0f1b2af1fbdb2aeacbb35bdec4880758a93f5e4
Carter <dj-ratmir@mail.ru>
2019-11-25 22:19:11 +0300

    [factions] Убрана проверка на организацию при взятии/кладении ящика в авто.

--- packages/factions/events.js
+++ packages/factions/events.js
@@ -29,7 +29,7 @@ module.exports = {
         var model = veh.db.modelName;
         if (veh.db.key != "faction") return notifs.error(player, `Авто ${model} не принадлежит организации`, header);
         var name = factions.getFaction(player.character.factionId).name;
-        if (veh.db.owner != player.character.factionId) return notifs.error(player, `Авто не принадлежит ${name}`, header);
+        // if (veh.db.owner != player.character.factionId) return notifs.error(player, `Авто не принадлежит ${name}`, header);
 
         if (player.hasAttachment("ammoBox")) {
             if (!factions.ammoVehModels.includes(model.toLowerCase())) return notifs.error(player, `Авто не предназначено для перевоза боеприпасов`, header);
@@ -64,7 +64,7 @@ module.exports = {
         var model = veh.db.modelName;
         if (veh.db.key != "faction") return notifs.error(player, `Авто ${model} не принадлежит организации`, header);
         var name = factions.getFaction(player.character.factionId).name;
-        if (veh.db.owner != player.character.factionId) return notifs.error(player, `Авто не принадлежит ${name}`, header);
+        // if (veh.db.owner != player.character.factionId) return notifs.error(player, `Авто не принадлежит ${name}`, header);
         if (!veh.products || !veh.products.count) return notifs.error(player, `Багажник пустой`, header);
         var type = veh.products.type;
         if (type != "ammo" && type != "medicines") return notifs.error(player, `Неверный тип товара`, header);
