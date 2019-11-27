94a33f4b157896ee86836c6c81e7742a4115bff1
Carter <dj-ratmir@mail.ru>
2019-11-27 17:00:47 +0300

    [anticheat] Анти-чит на побег из тюрьмы/КПЗ. Выключено оповещение игрока.

--- client_files/anticheat/index.js
+++ client_files/anticheat/index.js
@@ -10,6 +10,7 @@
 mp.anticheat = {
     // Параметры анти-чита
     weapon: false,
+    jail: false,
     // Анти-флуд
     lastTriggerTime: 0,
     waitTime: 10000,
@@ -23,11 +24,19 @@ mp.anticheat = {
 
         this.trigger(`weapon`, `Начитерил ган ${mp.weapons.getWeaponName(player.weapon) || ''}`);
     },
+    // анти-чит на побег из тюрьмы/КПЗ
+    checkJail() {
+        if (mp.police.arrestType != null) {
+            var info = mp.police.jailInfo[mp.police.arrestType];
+            var dist = mp.vdist(mp.players.local.position, info.coords);
+            if (dist > info.radius) this.trigger(`jail`, `Покинул зону тюрьмы/КПЗ`);
+        }
+    },
     trigger(name, reason) {
         if (Date.now() - this.lastTriggerTime < this.waitTime) return;
         this.lastTriggerTime = Date.now();
 
-        mp.notify.warning(reason, `ANTICHEAT`);
+        // mp.notify.warning(reason, `ANTICHEAT`);
         mp.events.callRemote(`anticheat.trigger`, name, reason);
     },
     enableParam(name, enable) {
@@ -46,5 +55,8 @@ mp.events.add({
     },
     "anticheat.param.set": (name, enable) => {
         mp.anticheat.enableParam(name, enable);
+    },
+    "time.main.tick": () => {
+        mp.anticheat.checkJail();
     },
 });
