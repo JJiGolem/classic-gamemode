c5cbdf3ce798510f60795f4a2692052364babb5f
Carter <dj-ratmir@mail.ru>
2019-12-03 17:46:52 +0300

    [time] Перезагрузка секундного и минутного таймера по WASD, если сломан. [police] Фикс пустого списка сотрудников в планшете при авторизации.

--- packages/police/events.js
+++ packages/police/events.js
@@ -17,15 +17,15 @@ module.exports = {
     },
     "characterInit.done": (player) => {
         player.call(`police.wanted.set`, [player.character.wanted]);
-        if (!player.character.arrestTime) return;
-
         var time = player.character.arrestTime;
-        if (player.character.arrestType == 0) police.startLSCellArrest(player, null, time);
-        else if (player.character.arrestType == 1) police.startJailArrest(player, null, time);
-        else if (player.character.arrestType == 2) police.startBCCellArrest(player, null, time);
+        if (time) {
+            if (player.character.arrestType == 0) police.startLSCellArrest(player, null, time);
+            else if (player.character.arrestType == 1) police.startJailArrest(player, null, time);
+            else if (player.character.arrestType == 2) police.startBCCellArrest(player, null, time);
+        }
 
-        if (!factions.isPoliceFaction(player.character.factionId)) return;
-        mp.events.call(`mapCase.pd.init`, player);
+        if (factions.isPoliceFaction(player.character.factionId))
+            mp.events.call(`mapCase.pd.init`, player);
     },
     "police.storage.clothes.take": (player, index) => {
         if (!player.insideFactionWarehouse) return notifs.error(player, `Вы далеко`, `Склад Police`);
