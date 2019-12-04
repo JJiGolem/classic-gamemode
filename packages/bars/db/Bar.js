23f9d5a5bd68749e5b8036e712b13f095c896439
Данил Аникьев <anikyev95@mail.ru>
2019-12-04 14:59:05 +0400

    [bars] Модуль баров

---
+++ packages/bars/db/Bar.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+    const model = sequelize.define("Bar", {
+        id: {
+            type: DataTypes.INTEGER(11),
+            primaryKey: true,
+            autoIncrement: true
+        },
+        x: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        y: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        z: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        }
+    }, { timestamps: false });
+
+    return model;
+};
